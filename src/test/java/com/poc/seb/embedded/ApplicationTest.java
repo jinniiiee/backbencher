4a96da4f90fa7b003a5ef0d4c9eb2e5efbe9babf
Isha Saini <isha.saini@sebx.se>
2024-02-29 14:43:19 +0100

    add new api

--- src/test/java/com/poc/seb/embedded/ApplicationTest.java
+++ src/test/java/com/poc/seb/embedded/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.com.seb.embedded;
+package com.poc.seb.embedded;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,7 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApplicationTest {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {}
 }
